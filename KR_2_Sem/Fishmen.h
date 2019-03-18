00a2a4815e98a58de43f018c7dc7ca6f4f2ed7a0
Gleb Lagutko <gleblagutko@gmail.com>
2019-03-18 11:31:06 +0300

    Parasha

---
+++ KR_2_Sem/Fishmen.h
@@ -0,0 +1,153 @@
+#pragma once
+#pragma once
+#include <iostream>
+#include <vector>
+#include <ostream>
+#include <iterator>
+#include <sstream>
+#include <map>
+
+using namespace std;
+
+class Fishmen
+{
+public:
+	wstring name;
+	wstring surname;
+	multimap<wstring, double> fishes;
+	int years;
+	double summa;
+
+	Fishmen() {}
+
+	Fishmen(const Fishmen& other)
+		: name(other.name),
+		surname(other.surname),
+		fishes(other.fishes),
+		years(other.years),
+		summa(other.summa)
+	{
+	}
+
+	Fishmen(Fishmen&& other)
+		: name(std::move(other.name)),
+		surname(std::move(other.surname)),
+		fishes(std::move(other.fishes)),
+		years(std::move(other.years)),
+		summa(std::move(other.summa))
+	{
+	}
+
+	void SetYears(int a)
+	{
+		this->years = a;
+	}
+
+	int GetYears()
+	{
+		return this->years;
+	}
+
+	void SetSumma(int a)
+	{
+		this->summa = a;
+	}
+
+	int GetSumma()
+	{
+		return this->summa;
+	}
+
+	wstring GetName() const
+	{
+		return name;
+	}
+
+	void SetName(wstring name)
+	{
+		this->name = name;
+	}
+
+
+	wstring GetSurname() const
+	{
+		return surname;
+	}
+
+	void SetSurname(wstring surname)
+	{
+		this->surname = surname;
+	}
+
+
+
+	multimap<wstring, double> GetFishes() const
+	{
+		return fishes;
+	}
+
+	void SetFishes(multimap<wstring, double> subject)
+	{
+		this->fishes = subject;
+	}
+
+
+
+	friend bool operator==(const Fishmen& lhs, const Fishmen& rhs)
+	{
+		return lhs.name == rhs.name
+			&& lhs.surname == rhs.surname
+			&& lhs.fishes == rhs.fishes
+			&& lhs.years == rhs.years
+			&& lhs.summa == rhs.summa;
+	}
+
+	friend bool operator!=(const Fishmen& lhs, const Fishmen& rhs)
+	{
+		return !(lhs == rhs);
+	}
+
+	Fishmen& operator=(const Fishmen& other)
+	{
+		if (this == &other)
+			return *this;
+		name = other.name;
+		surname = other.surname;
+		fishes = other.fishes;
+		years = other.years;
+		summa = other.summa;
+		return *this;
+	}
+
+	Fishmen& operator=(Fishmen&& other)
+	{
+		if (this == &other)
+			return *this;
+		name = std::move(other.name);
+		surname = std::move(other.surname);
+		fishes = std::move(other.fishes);
+		years = std::move(other.years);
+		summa = std::move(other.summa);
+		return *this;
+	}
+
+
+	friend bool operator<(const Fishmen& lhs, const Fishmen& rhs)
+	{
+		return lhs.summa < rhs.summa;
+	}
+
+
+
+	friend bool operator>(const Fishmen& lhs, const Fishmen& rhs)
+	{
+		return rhs < lhs;
+	}
+
+
+	friend std::wostream& operator<<(std::wostream& os, const Fishmen& obj)
+	{
+		return os << obj.name << " " << obj.surname << " " << obj.years << " " << obj.summa;
+	}
+
+};
---
+++ KR_2_Sem/KR.cpp
@@ -0,0 +1,147 @@
+#include <iostream>
+#include <vector>
+#include <ostream>
+#include <iterator>
+#include <sstream>
+#include <map>
+#include <fstream>
+#include "Fishmen.h"
+#include <queue>
+#include <codecvt>
+
+using namespace std;
+
+void FoutSort(multimap<int, Fishmen>& mapFishers1, multimap<int, Fishmen>& mapFishers2, wofstream& fout)
+{
+	for (auto a : mapFishers2)
+		mapFishers1.insert(pair<int, Fishmen>(a.first, a.second));
+	for (auto a : mapFishers1)
+		fout << a.second << endl;
+}
+
+void LastTable(multimap<int, Fishmen>& mapFishers1, multimap<int, Fishmen>& mapFishers2, wofstream& fout)
+{
+	for (auto a : mapFishers2)
+		mapFishers1.insert(pair<int, Fishmen>(a.first, a.second));
+	for (auto it = mapFishers1.begin(); it != mapFishers1.upper_bound(17); ++it)
+		it->second.summa *= 1.1;
+	for (auto it = mapFishers1.lower_bound(18); it != mapFishers1.upper_bound(30); ++it)
+		it->second.summa *= 1.0;
+	for (auto it = mapFishers1.lower_bound(18); it != mapFishers1.upper_bound(30); ++it)
+		it->second.summa *= 0.9;
+	for_each(mapFishers1.begin(), mapFishers1.end(), [&](auto& a) {fout << a.second << endl; });
+}
+
+void FindMaxByYears(multimap<int, Fishmen>& mapFishmen, wofstream& fout)
+{
+	priority_queue<Fishmen> queueFishers;
+	fout << L"Under 18 : ";
+
+	for (auto it = mapFishmen.begin(); it != mapFishmen.upper_bound(17); ++it)
+		queueFishers.push(it->second);
+	if (!queueFishers.empty())
+		fout << queueFishers.top().name << " " << queueFishers.top().surname << endl;
+	else
+		fout << L" Nobody " << endl;
+	for (auto a = 0; a < queueFishers.size(); a++)
+		queueFishers.pop();
+
+	fout << L"From 18 to 30 : ";
+	for (auto it = mapFishmen.lower_bound(18); it != mapFishmen.upper_bound(30); ++it)
+		queueFishers.push(it->second);
+	if (!queueFishers.empty())
+		fout << queueFishers.top().name << " " << queueFishers.top().surname << endl;
+	else
+		fout << L" Nobody " << endl;
+	for (auto a = 0; a < queueFishers.size(); a++)
+		queueFishers.pop();
+
+	fout << L"From 30 : ";
+	for (auto it = mapFishmen.lower_bound(30); it != mapFishmen.end(); ++it)
+		queueFishers.push(it->second);
+	if (!queueFishers.empty())
+		fout << queueFishers.top().name << " " << queueFishers.top().surname << endl;
+	else
+		fout << L" Nobody " << endl;
+	fout << endl;
+}
+
+multimap<int,Fishmen> FillMapYears(vector<Fishmen>& VectorFishmen)
+{
+	multimap<int, Fishmen> multiMap;
+	for_each(VectorFishmen.begin(),VectorFishmen.end(), [&](const auto& a) {multiMap.insert(pair<int, Fishmen>(a.years, a));});
+	return multiMap;
+}
+
+void ShowAll(vector<Fishmen>& VectorFishmen1, vector<Fishmen>& VectorFishmen2,wofstream& fout)
+{
+	fout << L"All : " << endl;
+	for_each(VectorFishmen1.begin(), VectorFishmen1.end(), [&](auto& a) {fout << a << endl;  });
+	for_each(VectorFishmen2.begin(), VectorFishmen2.end(), [&](auto& a) {fout << a << endl;  });
+	fout << endl;
+}
+
+
+vector<Fishmen> FillVector(string way)
+{
+
+		vector<Fishmen> FishmenVector;
+		wifstream wifile;
+		wifile.open(way);
+		wifile.imbue(locale(locale::empty(), new codecvt_utf8<wchar_t>));
+		Fishmen men;
+		while (!wifile.eof())
+		{
+			wstring wstr;
+			wstring name;
+			double weight;
+			getline(wifile, wstr);
+			wstringstream str_stream(wstr);
+			str_stream >> men.name >> men.surname >> men.years;
+			getline(wifile, wstr);
+			wstringstream str_stream1(wstr);
+			while (!str_stream1.eof())
+			{
+				str_stream1 >> name >> weight;
+				men.fishes.insert(pair<wstring, double>(name, weight));
+			}
+			double sum = 0;
+			for(auto a : men.fishes)
+			{
+				sum = sum + a.second;
+			}
+			men.summa = sum;
+		    FishmenVector.push_back(men);
+		}
+
+		return FishmenVector;
+}
+
+void Menu(string way1,string way2)
+{
+	auto VectorFishmen2017 = FillVector(way1);
+	auto VectorFishmen2018 = FillVector(way2);
+	
+	wofstream fout("C:\\Users\\Dell\\source\\repos\\KR_2_Sem\\output.txt");
+	fout.imbue(locale(locale::empty(), new codecvt_utf8<wchar_t>));
+	auto MapFishmen2017 = FillMapYears(VectorFishmen2017);
+	auto MapFishmen2018 = FillMapYears(VectorFishmen2018);
+	fout << L"Best in 2017 : " << endl;
+	FindMaxByYears(MapFishmen2017, fout);
+	fout << L"Best in 2018 : " << endl;
+	FindMaxByYears(MapFishmen2018, fout);
+	ShowAll(VectorFishmen2017,VectorFishmen2018, fout);
+
+	fout << L"Sorted : " << endl;
+	FoutSort(MapFishmen2017, MapFishmen2018, fout);
+	fout << endl;
+	fout << L"Last Table : " << endl;
+	LastTable(MapFishmen2017, MapFishmen2018, fout);
+}
+
+int main()
+{
+	Menu("C:\\Users\\Dell\\source\\repos\\KR_2_Sem\\2017.txt", "C:\\Users\\Dell\\source\\repos\\KR_2_Sem\\2018.txt");
+	system("pause");
+
+}
